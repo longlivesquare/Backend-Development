5c2abb0c2ee2953af76672bc86aad592aea48c99
Steven Duran <stevenduran@gmail.com>
2021-04-21 15:53:47 -0700

    Added default react app for future frontend

--- FinalProject/FinalProject/boardgames/admin.py
+++ FinalProject/FinalProject/boardgames/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
-from .models import Boardgame,Category
+from .models import Boardgame,Category,Play,Player,Play_To_Player,Designer,Boardgame_Has_Designer
 
+class BoardgameDesignerAdmin(admin.ModelAdmin):
+    list_display = ('boardgame_id', 'designer_id')
+
+class PlayToPlayerAdmin(admin.ModelAdmin):
+    list_display = ('play_id', 'player_id')
+
+class PlayAdmin(admin.ModelAdmin):
+    list_display = ('boardgame_id', 'date', 'winning_player_id')
+    date_hierarchy = 'date'
 # Register your models here.
 
 admin.site.register(Boardgame)
 admin.site.register(Category)
+admin.site.register(Play, PlayAdmin)
+admin.site.register(Player)
+admin.site.register(Play_To_Player, PlayToPlayerAdmin)
+admin.site.register(Designer)
+admin.site.register(Boardgame_Has_Designer, BoardgameDesignerAdmin)
--- FinalProject/FinalProject/boardgames/models.py
+++ FinalProject/FinalProject/boardgames/models.py
@@ -9,11 +9,36 @@ class Category(models.Model):
 
 class Boardgame(models.Model):
     name = models.CharField(max_length=256)
-    year_published = models.IntegerField()
-    min_players = models.IntegerField()
-    max_players = models.IntegerField()
-    edition = models.FloatField()
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
+    year_published = models.IntegerField(null=True, blank=True)
+    min_players = models.IntegerField(null=True, blank=True)
+    max_players = models.IntegerField(null=True, blank=True)
+    edition = models.FloatField(null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.name
+
+class Player(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+class Play(models.Model):
+    boardgame_id = models.ForeignKey(Boardgame, on_delete=models.CASCADE)
+    date = models.DateField(null=True, blank=True)
+    winning_player_id = models.ForeignKey(Player, null=True, blank=True, on_delete=models.CASCADE)
+
+class Designer(models.Model):
+    name = models.CharField(max_length=48)
+
+    def __str__(self):
+        return self.name
+
+class Play_To_Player(models.Model):
+    play_id = models.ForeignKey(Play, on_delete=models.CASCADE)
+    player_id = models.ForeignKey(Player, on_delete=models.CASCADE)
+
+class Boardgame_Has_Designer(models.Model):
+    boardgame_id = models.ForeignKey(Boardgame, on_delete=models.CASCADE)
+    designer_id = models.ForeignKey(Designer, on_delete=models.CASCADE)
